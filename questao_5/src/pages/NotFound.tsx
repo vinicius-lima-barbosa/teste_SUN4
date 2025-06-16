65f892be0b54b095b85bb3c8cc6bae116aec099b
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-06-16 11:58:10 -0300

    style: add the 'NotFound' page style

--- questao_5/src/pages/NotFound.tsx
+++ questao_5/src/pages/NotFound.tsx
@@ -1,7 +1,15 @@
 const NotFound = () => {
   return (
-    <div>
-      <h1>Page Not found</h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="font-bold text-3xl text-indigo-600">
+          404 - Page Not found
+        </h1>
+        <p className="text-xl">The page you are looking for does not exist.</p>
+        <a href="/" className="text-lg text-blue-500 hover:underline">
+          Return the home page!
+        </a>
+      </div>
     </div>
   );
 };
