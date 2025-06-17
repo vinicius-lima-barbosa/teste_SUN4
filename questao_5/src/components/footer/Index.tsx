7accd1b5e67a68106d9af6bb7321418c6bf7d83b
vinicius-lima-barbosa <viniciusbarbosa0202@gmail.com>
2025-06-16 22:56:21 -0300

    feat: add Footer component

---
+++ questao_5/src/components/footer/Index.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-50 border-t border-gray-200 text-center py-8 mt-20 text-gray-500 text-sm">
+      &copy; {new Date().getFullYear()} Vinicius Barbosa. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
--- questao_5/src/pages/Index.tsx
+++ questao_5/src/pages/Index.tsx
@@ -1,4 +1,5 @@
 import ContactSection from "../components/contact-section/Index";
+import Footer from "../components/footer/Index";
 import HeroSection from "../components/hero-section/Index";
 import Navigation from "../components/navigation/Index";
 import Plans from "../components/plans-section/Index";
@@ -10,6 +11,7 @@ const Index = () => {
       <HeroSection />
       <Plans />
       <ContactSection />
+      <Footer />
     </>
   );
 };
